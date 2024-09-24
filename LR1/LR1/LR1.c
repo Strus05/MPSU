ef2fc8c01ada597768895213eb4e514f3f73a6b9
Strus05 <84398480+Strus05@users.noreply.github.com>
2024-09-24 16:17:36 +0300

    Исправить ATmel и proteus
    Сменил порты PB0-2 на PB5-7. Улучшил читаемость схемы в proteus

--- LR1/LR1/LR1.c
+++ LR1/LR1/LR1.c
@@ -5,7 +5,7 @@ int main(void)
 	DDRC |= (1<<PC7);
 	while(1)
 	{
-		if ((PINB & (1<<PB0)) || (PINB & (1<<PB1)) || (PINB & (1<<PB2))) // PINB || 0b00000111)
+		if ((PINB & (1<<PB5)) || (PINB & (1<<PB6)) || (PINB & (1<<PB7))) // PINB || 0b11100000
 		{
 			PORTC |= (1<<PC7);
 		}
